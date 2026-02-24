86a2019a5b48851d1b2afec43662ff3a8c88b62c
Shubham <bhattacharya.shubham123@gmail.com>
2026-02-23 23:44:49 -0600

    added a get mapping endpoint

---
+++ quickstart/src/main/java/com/example/quickstart/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWold() {
+        return "Hello world";
+    }
+
+}
